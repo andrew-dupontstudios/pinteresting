8f55a741b7178d45f8bde65ae4f0b120dc52c752
Andrew Klubertanz <andrew@dupontstudios.com>
2014-11-21 14:23:53 -0500

    Images Fixed and Timestamp removed

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,5 @@
 class Pin < ActiveRecord::Base
   belongs_to :user
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },:use_timestamp => false
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
